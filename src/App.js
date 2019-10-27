d872f20944b37b75d62a30f4d31c24559c24bea0
Param Harrison <paramanantham@live.com>
2019-10-27 19:43:53 +0200

    feat: simple for with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full max-w-md mx-auto bg-gray-800">
+      <form className=" bg-white rounded px-8 py-8 pt-8">
+        <div className="px-4 pb-4">
+          <h1 className="uppercase font-bold text-xl">Asana inline edit UI</h1>
+        </div>
+        <div className="px-4 pb-4">
+          <input
+            type="text"
+            name="task"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
+            placeholder="Write a task name"
+          />
+        </div>
+        <div className="px-4 pb-4">
+          <textarea
+            name="description"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
+            placeholder="Description"
+            rows="5"
+          />
+        </div>
+      </form>
     </div>
   );
 }
