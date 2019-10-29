32d76c54eecb5252291788a035aac2b1fb6ad981
Param Harrison <paramanantham@live.com>
2019-10-29 20:54:15 +0200

    fix: editable ui with basic styles

--- src/Editable.js
+++ src/Editable.js
@@ -17,30 +17,36 @@ const Editable = ({
     }
   }, [isEditing, childRef]);
 
-  const handleKeyDown = event => {
+  const handleKeyDown = (event, type) => {
     const { key } = event;
-    switch (key) {
-      case "Enter":
-      case "Escape":
-        setEditing(false);
-        break;
-      default:
-        break;
+    const keys = ["Escape", "Tab"];
+    const enterKey = "Enter";
+    const allKeys = [...keys, enterKey];
+    if (
+      (type === "textarea" && keys.indexOf(key) > -1) ||
+      (type !== "textarea" && allKeys.indexOf(key) > -1)
+    ) {
+      setEditing(false);
     }
   };
 
   return (
     <section {...props}>
       {isEditing ? (
-        <div onBlur={() => setEditing(false)} onKeyDown={handleKeyDown}>
+        <div
+          onBlur={() => setEditing(false)}
+          onKeyDown={e => handleKeyDown(e, type)}
+        >
           {children}
         </div>
       ) : (
         <div
-          className={`rounded py-2 px-3 text-gray-700 leading-tight hover:shadow-outline editable-${type}`}
+          className={`rounded py-2 px-3 text-gray-700 leading-tight whitespace-pre-wrap hover:shadow-outline editable-${type}`}
           onClick={() => setEditing(true)}
         >
-          {text || placeholder || "Editable content"}
+          <span className={`${text ? "text-black" : "text-gray-500"}`}>
+            {text || placeholder || "Editable content"}
+          </span>
         </div>
       )}
     </section>
