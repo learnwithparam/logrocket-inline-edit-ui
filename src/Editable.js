99319d8c25efbc371428acd70725729494080de9
Paramanantham Harrison <paramanantham@live.com>
2019-10-29 20:54:59 +0200

    feat: style for the editable UI (#2)
    * styling for editable content
    * fix: more styles to fix UI glitches
    * fix: editable UI with basic styles

--- src/Editable.js
+++ src/Editable.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import "./Editable.css";
 
-const Editable = ({ text, placeholder, children, childRef, ...props }) => {
+const Editable = ({
+  text,
+  type,
+  placeholder,
+  children,
+  childRef,
+  ...props
+}) => {
   const [isEditing, setEditing] = useState(false);
 
   useEffect(() => {
@@ -10,27 +17,36 @@ const Editable = ({ text, placeholder, children, childRef, ...props }) => {
     }
   }, [isEditing, childRef]);
 
-  const handleKeyDown = event => {
+  const handleKeyDown = (event, type) => {
     const { key } = event;
-    switch (key) {
-      case "Enter":
-      case "Escape":
-        setEditing(false);
-        break;
-      default:
-        break;
+    const keys = ["Escape", "Tab"];
+    const enterKey = "Enter";
+    const allKeys = [...keys, enterKey];
+    if (
+      (type === "textarea" && keys.indexOf(key) > -1) ||
+      (type !== "textarea" && allKeys.indexOf(key) > -1)
+    ) {
+      setEditing(false);
     }
   };
 
   return (
     <section {...props}>
       {isEditing ? (
-        <div onBlur={() => setEditing(false)} onKeyDown={handleKeyDown}>
+        <div
+          onBlur={() => setEditing(false)}
+          onKeyDown={e => handleKeyDown(e, type)}
+        >
           {children}
         </div>
       ) : (
-        <div onClick={() => setEditing(true)}>
-          {text || placeholder || "Editable content"}
+        <div
+          className={`rounded py-2 px-3 text-gray-700 leading-tight whitespace-pre-wrap hover:shadow-outline editable-${type}`}
+          onClick={() => setEditing(true)}
+        >
+          <span className={`${text ? "text-black" : "text-gray-500"}`}>
+            {text || placeholder || "Editable content"}
+          </span>
         </div>
       )}
     </section>
